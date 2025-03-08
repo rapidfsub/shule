16af9fb508d8295df567b2023f78508ae5df30dd
Minsub Kim <rapidfsub@gmail.com>
2025-03-08 16:49:22 +0900

    add arity option

--- victor/lib/victor/mixin.ex
+++ victor/lib/victor/mixin.ex
@@ -1,18 +1,21 @@
 defmodule Victor.Mixin do
   alias Victor.Mixin.FunInfo
 
-  defmacro delegate_to(mod, fname, arity, opts \\ []) do
+  defmacro delegate_to(mod, fname, opts \\ []) do
     {mod, _bindings} = Code.eval_quoted(mod)
-    {:ok, info} = FunInfo.new(mod, fname, arity)
-    dname = Keyword.get(opts, :alias, info.name)
-    opts = [to: info.mod, as: info.name]
+    dname = Keyword.get(opts, :alias, fname)
+    arity_or_nil = Keyword.get(opts, :arity)
 
-    for arity <- (info.arity - info.defaults)..info.arity//1 do
-      args = Enum.take(info.args, arity)
+    for info <- FunInfo.list_infos(mod, fname) do
+      opts = [to: info.mod, as: info.name]
 
-      quote do
-        @doc unquote(info.doc)
-        defdelegate unquote({dname, [], args}), unquote(opts)
+      for arity <- (info.arity - info.defaults)..info.arity//1, arity_or_nil in [nil, arity] do
+        args = Enum.take(info.args, arity)
+
+        quote do
+          @doc unquote(info.doc)
+          defdelegate unquote({dname, [], args}), unquote(opts)
+        end
       end
     end
   end
