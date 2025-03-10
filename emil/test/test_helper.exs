a927e1888bd171e74a60c0ebea5266a781b4a528
Minsub Kim <rapidfsub@gmail.com>
2025-03-10 14:50:52 +0900

    setup test repo

--- emil/test/test_helper.exs
+++ emil/test/test_helper.exs
@@ -1,6 +1,9 @@
 ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(Emil.Repo, :manual)
 
+Emil.TestRepo.start_link()
+Ecto.Adapters.SQL.Sandbox.mode(Emil.TestRepo, :manual)
+
 AshOban.config([Emil.TestDomain],
   engine: Oban.Engines.Basic,
   notifier: Oban.Notifiers.Postgres,
