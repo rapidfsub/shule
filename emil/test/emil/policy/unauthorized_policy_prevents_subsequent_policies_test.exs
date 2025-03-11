3936c28ee3f0f505a037b8fd0a3fdcbb381f641b
Minsub Kim <rapidfsub@gmail.com>
2025-03-11 13:46:42 +0900

    test unauthorized policy

---
+++ emil/test/emil/policy/unauthorized_policy_prevents_subsequent_policies_test.exs
@@ -0,0 +1,47 @@
+alias Emil.Policy.UnauthorizedPolicyPreventsSubsequentPoliciesTest, as: ThisTest
+
+defmodule ThisTest.Obj do
+  use Ash.Resource,
+    authorizers: [Ash.Policy.Authorizer],
+    domain: Emil.TestDomain
+
+  actions do
+    defaults [:read, create: :*]
+  end
+
+  policies do
+    policy always() do
+      authorize_if expr(^actor(:is_admin))
+    end
+
+    policy always() do
+      authorize_if always()
+    end
+  end
+
+  attributes do
+    uuid_v7_primary_key :id
+    attribute :d1, :decimal, allow_nil?: false, public?: true
+  end
+end
+
+defmodule ThisTest do
+  use ExUnit.Case, async: true
+
+  test "an unauthorized policy prevents subsequent policies from being evaluated" do
+    params = %{d1: 1}
+    assert {:error, _} = Ash.Changeset.for_create(ThisTest.Obj, :create, params) |> Ash.create()
+    opts = [actor: %{}]
+
+    assert {:error, _} =
+             Ash.Changeset.for_create(ThisTest.Obj, :create, params, opts) |> Ash.create()
+
+    opts = [actor: %{is_admin: false}]
+
+    assert {:error, _} =
+             Ash.Changeset.for_create(ThisTest.Obj, :create, params, opts) |> Ash.create()
+
+    opts = [actor: %{is_admin: true}]
+    assert Ash.Changeset.for_create(ThisTest.Obj, :create, params, opts) |> Ash.create!()
+  end
+end
