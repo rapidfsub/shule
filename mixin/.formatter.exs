c28e79a0ec851ce9b270d647c18771cebcef3f71
Minsub Kim <rapidfsub@gmail.com>
2025-03-14 10:06:15 +0900

    configure formatter

--- mixin/.formatter.exs
+++ mixin/.formatter.exs
@@ -1,4 +1,8 @@
 # Used by "mix format"
+locals_without_parens = [delegate_to: 1, delegate_to: 2, delegate_to: 3]
+
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  locals_without_parens: locals_without_parens,
+  export: [locals_without_parens: locals_without_parens]
 ]
