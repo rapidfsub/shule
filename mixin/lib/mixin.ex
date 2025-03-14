45b622f4a64a88fc6b141ac661228bcebd4756a1
Minsub Kim <rapidfsub@gmail.com>
2025-03-14 10:01:42 +0900

    mix new mixin

---
+++ mixin/lib/mixin.ex
@@ -0,0 +1,18 @@
+defmodule Mixin do
+  @moduledoc """
+  Documentation for `Mixin`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Mixin.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
