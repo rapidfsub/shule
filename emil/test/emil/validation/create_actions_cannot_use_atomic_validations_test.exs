017cfa4ee811a46a13386ccb6747024bd89e4f3d
Minsub Kim <rapidfsub@gmail.com>
2025-03-22 14:06:00 +0900

    test create action atomic validation

---
+++ emil/test/emil/validation/create_actions_cannot_use_atomic_validations_test.exs
@@ -0,0 +1,40 @@
+use Emil.TestPrelude
+alias Emil.Validation.CreateActionsCannotUseAtomicValidationsTest, as: ThisTest
+
+defmodule ThisTest.Validation do
+  use Ash.Resource.Validation
+
+  @impl Ash.Resource.Validation
+  def atomic(changeset, _opts, _context) do
+    {:atomic, [:d1, :d2], expr(d1 + d2 < 10), "invalid"}
+  end
+end
+
+defmodule ThisTest.Obj do
+  use Ash.Resource,
+    data_layer: Ash.DataLayer.Ets,
+    domain: TestDomain
+
+  actions do
+    defaults [:read, create: :*]
+  end
+
+  validations do
+    validate ThisTest.Validation
+  end
+
+  attributes do
+    uuid_v7_primary_key :id
+    attribute :d1, :decimal, allow_nil?: false, public?: true
+    attribute :d2, :decimal, allow_nil?: false, public?: true
+  end
+end
+
+defmodule ThisTest do
+  use ExUnit.Case, async: true
+
+  test "create actions cannot use atomic validations" do
+    changeset = Changeset.for_create(ThisTest.Obj, :create, %{d1: 10, d2: 10})
+    assert {:error, %Ash.Error.Framework{}} = Ash.create(changeset)
+  end
+end
