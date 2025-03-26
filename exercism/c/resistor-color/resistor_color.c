84d96fb7e380b66326128c41a404a591212af956
Minsub Kim <rapidfsub@gmail.com>
2025-03-26 14:26:52 +0900

    solve resistor-color

--- exercism/c/resistor-color/resistor_color.c
+++ exercism/c/resistor-color/resistor_color.c
@@ -1,1 +1,18 @@
 #include "resistor_color.h"
+#include <stdlib.h>
+
+const resistor_band_t COLORS[] = {BLACK, BROWN, RED,    ORANGE, YELLOW,
+                                  GREEN, BLUE,  VIOLET, GREY,   WHITE};
+
+const resistor_band_t *colors(void) { return COLORS; }
+
+int color_code(resistor_band_t band) {
+  int result = 0;
+  while (result < 10) {
+    if (COLORS[result] == band) {
+      return result;
+    }
+    result += 1;
+  }
+  exit(1);
+}
--- exercism/c/resistor-color/resistor_color.h
+++ exercism/c/resistor-color/resistor_color.h
@@ -2,7 +2,19 @@
 #define RESISTOR_COLOR_H
 
 typedef enum {
-
+  BLACK,
+  BROWN,
+  RED,
+  ORANGE,
+  YELLOW,
+  GREEN,
+  BLUE,
+  VIOLET,
+  GREY,
+  WHITE
 } resistor_band_t;
 
 #endif
+
+const resistor_band_t *colors(void);
+int color_code(resistor_band_t band);
