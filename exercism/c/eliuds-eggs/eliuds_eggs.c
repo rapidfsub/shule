b7316aab14ba7ec89bcddd1638474587805df1c3
Minsub Kim <rapidfsub@gmail.com>
2025-03-27 14:38:49 +0900

    solve eliuds-eggs

--- exercism/c/eliuds-eggs/eliuds_eggs.c
+++ exercism/c/eliuds-eggs/eliuds_eggs.c
@@ -1,1 +1,12 @@
 #include "eliuds_eggs.h"
+
+unsigned int egg_count(unsigned int n) {
+  unsigned int result = 0;
+  while (n > 0) {
+    if (n % 2 == 1) {
+      result += 1;
+    }
+    n /= 2;
+  }
+  return result;
+}
