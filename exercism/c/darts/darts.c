9f6a78f8a3044091b9919d29e9dcc09ff07a92a2
Minsub Kim <rapidfsub@gmail.com>
2025-03-27 10:29:41 +0900

    solve darts

--- exercism/c/darts/darts.c
+++ exercism/c/darts/darts.c
@@ -1,1 +1,14 @@
 #include "darts.h"
+
+uint8_t score(coordinate_t pos) {
+  float dist = pow(pos.x, 2) + pow(pos.y, 2);
+  if (dist > 100) {
+    return 0;
+  } else if (dist > 25) {
+    return 1;
+  } else if (dist > 1) {
+    return 5;
+  } else {
+    return 10;
+  }
+}
