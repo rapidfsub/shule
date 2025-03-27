515e0b1de90d10189805a5485e08e301ef7f45a7
Minsub Kim <rapidfsub@gmail.com>
2025-03-27 14:47:08 +0900

    solve two-fer

--- exercism/c/two-fer/two_fer.c
+++ exercism/c/two-fer/two_fer.c
@@ -1,1 +1,11 @@
 #include "two_fer.h"
+
+void two_fer(char *buffer, const char *name) {
+  strcat(buffer, "One for ");
+  if (name == NULL) {
+    strcat(buffer, "you");
+  } else {
+    strcat(buffer, name);
+  }
+  strcat(buffer, ", one for me.");
+}
