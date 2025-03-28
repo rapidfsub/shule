83afff7f69fd9dd3af147b8456fda4e0f4a61be6
Minsub Kim <rapidfsub@gmail.com>
2025-03-28 19:34:23 +0900

    solve isogram

--- exercism/c/isogram/isogram.c
+++ exercism/c/isogram/isogram.c
@@ -1,1 +1,33 @@
 #include "isogram.h"
+
+int to_bit(char letter);
+
+bool is_isogram(const char phrase[]) {
+  if (phrase == NULL) {
+    return false;
+  }
+
+  size_t len = strlen(phrase);
+  int set = 0;
+  for (size_t i = 0; i < len; i += 1) {
+    int bit = to_bit(phrase[i]);
+    if ((set & bit) > 0) {
+      return false;
+    } else {
+      set |= bit;
+    }
+  }
+  return true;
+}
+
+int to_bit(char letter) {
+  if (letter >= 'a' && letter <= 'z') {
+    return 1 << (letter - 'a');
+  } else if (letter >= 'A' && letter <= 'Z') {
+    return 1 << (letter - 'A');
+  } else if (letter >= ' ' && letter <= '-') {
+    return 0;
+  } else {
+    exit(1);
+  }
+}
