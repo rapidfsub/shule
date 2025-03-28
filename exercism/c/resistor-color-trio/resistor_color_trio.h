44576bf282aaf3cafedea42340180ff46b000a4d
Minsub Kim <rapidfsub@gmail.com>
2025-03-28 14:14:32 +0900

    solve resistor-color-trio

--- exercism/c/resistor-color-trio/resistor_color_trio.h
+++ exercism/c/resistor-color-trio/resistor_color_trio.h
@@ -1,4 +1,34 @@
 #ifndef RESISTOR_COLOR_TRIO_H
 #define RESISTOR_COLOR_TRIO_H
 
+#include <stdint.h>
+#include <stdlib.h>
+
+typedef enum {
+  BLACK,
+  BROWN,
+  RED,
+  ORANGE,
+  YELLOW,
+  GREEN,
+  BLUE,
+  VIOLET,
+  GREY,
+  WHITE
+} resistor_band_t;
+
+typedef enum {
+  OHMS,
+  KILOOHMS,
+  MEGAOHMS,
+  GIGAOHMS,
+} resistor_unit_t;
+
+typedef struct {
+  uint64_t value;
+  resistor_unit_t unit;
+} resistor_value_t;
+
+resistor_value_t color_code(resistor_band_t *bands);
+
 #endif
