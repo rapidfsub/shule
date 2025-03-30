7f0ed4ce92353d441621a292efb90fd105bde359
Minsub Kim <rapidfsub@gmail.com>
2025-03-30 16:42:21 +0900

    solve triangle

--- exercism/c/triangle/triangle.c
+++ exercism/c/triangle/triangle.c
@@ -1,1 +1,33 @@
 #include "triangle.h"
+#include <stdlib.h>
+
+bool is_triangle(triangle_t t);
+int cmp(const void *lhs, const void *rhs);
+
+bool is_equilateral(triangle_t t) {
+  return is_triangle(t) && t.a == t.b && t.b == t.c;
+}
+
+bool is_isosceles(triangle_t t) {
+  return is_triangle(t) && (t.a == t.b || t.b == t.c || t.c == t.a);
+}
+
+bool is_scalene(triangle_t t) {
+  return is_triangle(t) && t.a != t.b && t.b != t.c && t.c != t.a;
+}
+
+bool is_triangle(triangle_t t) {
+  double sides[3] = {t.a, t.b, t.c};
+  qsort(sides, 3, sizeof(t.a), cmp);
+  return sides[0] + sides[1] > sides[2];
+}
+
+int cmp(const void *lhs, const void *rhs) {
+  if (*(double *)lhs < *(double *)rhs) {
+    return -1;
+  } else if (*(double *)lhs == *(double *)rhs) {
+    return 0;
+  } else {
+    return 1;
+  }
+}
