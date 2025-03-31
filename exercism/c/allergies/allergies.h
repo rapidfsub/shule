f4503479773ad441a5df1e0a91110b5d517bb455
Minsub Kim <rapidfsub@gmail.com>
2025-04-01 08:13:38 +0900

    solve allergies

--- exercism/c/allergies/allergies.h
+++ exercism/c/allergies/allergies.h
@@ -4,20 +4,23 @@
 #include <stdbool.h>
 
 typedef enum {
-   ALLERGEN_EGGS = 0,
-   ALLERGEN_PEANUTS,
-   ALLERGEN_SHELLFISH,
-   ALLERGEN_STRAWBERRIES,
-   ALLERGEN_TOMATOES,
-   ALLERGEN_CHOCOLATE,
-   ALLERGEN_POLLEN,
-   ALLERGEN_CATS,
-   ALLERGEN_COUNT,
+  ALLERGEN_EGGS = 0,
+  ALLERGEN_PEANUTS,
+  ALLERGEN_SHELLFISH,
+  ALLERGEN_STRAWBERRIES,
+  ALLERGEN_TOMATOES,
+  ALLERGEN_CHOCOLATE,
+  ALLERGEN_POLLEN,
+  ALLERGEN_CATS,
+  ALLERGEN_COUNT,
 } allergen_t;
 
 typedef struct {
-   int count;
-   bool allergens[ALLERGEN_COUNT];
+  int count;
+  bool allergens[ALLERGEN_COUNT];
 } allergen_list_t;
+
+bool is_allergic_to(allergen_t allergen, int bits);
+allergen_list_t get_allergens(int bits);
 
 #endif
