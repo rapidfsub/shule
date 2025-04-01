25c1410e132261fb99f8a53b0f8afcef5e25fd2d
Minsub Kim <rapidfsub@gmail.com>
2025-04-01 10:56:54 +0900

    solve sum-of-multiples

--- exercism/c/sum-of-multiples/sum_of_multiples.c
+++ exercism/c/sum-of-multiples/sum_of_multiples.c
@@ -1,1 +1,23 @@
 #include "sum_of_multiples.h"
+
+unsigned int sum(const unsigned int *factors, const size_t number_of_factors,
+                 const unsigned int limit) {
+  bool did_visit[limit];
+  for (size_t i = 0; i < limit; i += 1) {
+    did_visit[i] = false;
+    for (size_t j = 0; j < number_of_factors; j += 1) {
+      if (factors[j] != 0 && i % factors[j] == 0) {
+        did_visit[i] = true;
+        break;
+      }
+    }
+  }
+
+  int result = 0;
+  for (size_t i = 0; i < limit; i += 1) {
+    if (did_visit[i]) {
+      result += i;
+    }
+  }
+  return result;
+}
--- exercism/c/sum-of-multiples/sum_of_multiples.h
+++ exercism/c/sum-of-multiples/sum_of_multiples.h
@@ -1,6 +1,7 @@
 #ifndef SUM_OF_MULTIPLES_H
 #define SUM_OF_MULTIPLES_H
 
+#include <stdbool.h>
 #include <stddef.h>
 
 unsigned int sum(const unsigned int *factors, const size_t number_of_factors,
