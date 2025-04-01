431cca7af9d679126b992404420684d9a9b89552
Minsub Kim <rapidfsub@gmail.com>
2025-04-01 15:55:45 +0900

    solve square-root

--- exercism/c/square-root/square_root.c
+++ exercism/c/square-root/square_root.c
@@ -1,1 +1,14 @@
 #include "square_root.h"
+
+const size_t MAX_ITERATIONS = 10000;
+
+int square_root(int n) {
+  int result = n;
+  for (size_t i = 0; i < MAX_ITERATIONS; i += 1) {
+    result = (result + n / result) / 2;
+    if (result * result == n) {
+      return result;
+    }
+  }
+  exit(1);
+}
--- exercism/c/square-root/square_root.h
+++ exercism/c/square-root/square_root.h
@@ -1,4 +1,8 @@
 #ifndef SQUARE_ROOT_H
 #define SQUARE_ROOT_H
 
+#include <stdlib.h>
+
+int square_root(int n);
+
 #endif
