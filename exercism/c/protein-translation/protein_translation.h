b78039614a7784abb8fe210e968907fd2fe82221
Minsub Kim <rapidfsub@gmail.com>
2025-04-01 21:51:05 +0900

    solve protein-translation

--- exercism/c/protein-translation/protein_translation.h
+++ exercism/c/protein-translation/protein_translation.h
@@ -3,23 +3,27 @@
 
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdio.h>
+#include <string.h>
 
 #define MAX_PROTEINS 10
 
 typedef enum {
-   Methionine,
-   Phenylalanine,
-   Leucine,
-   Serine,
-   Tyrosine,
-   Cysteine,
-   Tryptophan,
+  Stop = -100,
+
+  Methionine = 0,
+  Phenylalanine,
+  Leucine,
+  Serine,
+  Tyrosine,
+  Cysteine,
+  Tryptophan,
 } protein_t;
 
 typedef struct {
-   bool valid;
-   size_t count;
-   protein_t proteins[MAX_PROTEINS];
+  bool valid;
+  size_t count;
+  protein_t proteins[MAX_PROTEINS];
 } proteins_t;
 
 proteins_t proteins(const char *const rna);
