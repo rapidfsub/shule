5556e03296033dae803cb4fe303c5e78f78644f2
Minsub Kim <rapidfsub@gmail.com>
2025-04-02 17:05:02 +0900

    solve all-your-base

--- exercism/c/all-your-base/all_your_base.c
+++ exercism/c/all-your-base/all_your_base.c
@@ -1,1 +1,29 @@
 #include "all_your_base.h"
+
+size_t rebase(int8_t *digits, int16_t input_base, int16_t output_base,
+              size_t input_length) {
+  if (input_base < 2 || output_base < 2 || input_length < 1) {
+    return 0;
+  }
+
+  int value = 0;
+  for (size_t i = 0; i < input_length; i += 1) {
+    if (digits[i] >= 0 && digits[i] < input_base) {
+      value *= input_base;
+      value += digits[i];
+    } else {
+      return 0;
+    }
+  }
+
+  if (value < 1) {
+    return 1;
+  }
+
+  size_t result = log2(value) / log2(output_base) + 1;
+  for (size_t i = 1; i <= result; i += 1) {
+    digits[result - i] = value % output_base;
+    value /= output_base;
+  }
+  return result;
+}
--- exercism/c/all-your-base/all_your_base.h
+++ exercism/c/all-your-base/all_your_base.h
@@ -1,6 +1,14 @@
 #ifndef ALL_YOUR_BASE_H
 #define ALL_YOUR_BASE_H
 
+#include <math.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
 #define DIGITS_ARRAY_SIZE 64
+
+size_t rebase(int8_t *digits, int16_t input_base, int16_t output_base,
+              size_t input_length);
 
 #endif
