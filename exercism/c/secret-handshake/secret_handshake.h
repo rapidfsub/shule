9043c5eed6693f22cc716bb1dc487e4fd8f03023
Minsub Kim <rapidfsub@gmail.com>
2025-04-10 11:10:07 +0900

    solve secret-handshake

--- exercism/c/secret-handshake/secret_handshake.h
+++ exercism/c/secret-handshake/secret_handshake.h
@@ -1,7 +1,10 @@
 #ifndef SECRET_HANDSHAKE_H
 #define SECRET_HANDSHAKE_H
 
-#include <stddef.h>
+// #include <stddef.h>
+// #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 const char **commands(size_t number);
 
