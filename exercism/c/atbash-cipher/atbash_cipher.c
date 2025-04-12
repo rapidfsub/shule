bc069d28567ce4657b1260b64d27eb2aefa29cfe
Minsub Kim <rapidfsub@gmail.com>
2025-04-12 15:05:05 +0900

    solve atbash-cipher

--- exercism/c/atbash-cipher/atbash_cipher.c
+++ exercism/c/atbash-cipher/atbash_cipher.c
@@ -1,1 +1,36 @@
 #include "atbash_cipher.h"
+
+char *crypt(const char *input, bool will_split);
+
+char *atbash_encode(const char *input) { return crypt(input, true); }
+
+char *atbash_decode(const char *input) { return crypt(input, false); }
+
+char *crypt(const char *input, bool will_split) {
+  size_t len = strlen(input);
+  char *result = calloc(len * 2, sizeof(char));
+  size_t pos = 0;
+  for (size_t i = 0; i < len; i += 1) {
+    char curr = input[i];
+    if (!isalnum(curr)) {
+      continue;
+    }
+
+    if (will_split && pos % 6 == 5) {
+      result[pos] = ' ';
+      pos += 1;
+    }
+
+    if (isalpha(curr)) {
+      if (isupper(curr)) {
+        curr = tolower(curr);
+      }
+      result[pos] = 'z' - curr + 'a';
+      pos += 1;
+    } else if (isdigit(curr)) {
+      result[pos] = curr;
+      pos += 1;
+    }
+  }
+  return result;
+}
--- exercism/c/atbash-cipher/atbash_cipher.h
+++ exercism/c/atbash-cipher/atbash_cipher.h
@@ -1,7 +1,9 @@
-#ifndef ATBASH_CIPHER_H
-#define ATBASH_CIPHER_H
+#pragma once
+
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 
 char *atbash_encode(const char *input);
 char *atbash_decode(const char *input);
-
-#endif
