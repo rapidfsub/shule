17d44ef824865af818e76fccdd2357bff2b01a89
Minsub Kim <rapidfsub@gmail.com>
2025-04-12 16:27:50 +0900

    solve pythagorean-triplet

--- exercism/c/pythagorean-triplet/pythagorean_triplet.c
+++ exercism/c/pythagorean-triplet/pythagorean_triplet.c
@@ -1,1 +1,23 @@
 #include "pythagorean_triplet.h"
+
+triplets_t *triplets_with_sum(size_t sum) {
+  size_t size = sizeof(triplets_t) + sum * sizeof(triplet_t);
+  triplets_t *result = malloc(size);
+  result->count = 0;
+
+  triplet_t curr = {.a = 0, .b = 0, .c = 0};
+  size_t a_lim = sum / 3;
+  for (curr.a = 1; curr.a <= a_lim; curr.a += 1) {
+    size_t b_lim = (sum - curr.a) / 2;
+    for (curr.b = curr.a; curr.b <= b_lim; curr.b += 1) {
+      curr.c = sum - curr.a - curr.b;
+      if (curr.a * curr.a + curr.b * curr.b == curr.c * curr.c) {
+        result->triplets[result->count] = curr;
+        result->count += 1;
+      }
+    }
+  }
+  return result;
+}
+
+void free_triplets(triplets_t *triplets) { free(triplets); }
--- exercism/c/pythagorean-triplet/pythagorean_triplet.h
+++ exercism/c/pythagorean-triplet/pythagorean_triplet.h
@@ -1,4 +1,18 @@
-#ifndef PYTHAGOREAN_TRIPLET_H
-#define PYTHAGOREAN_TRIPLET_H
+#pragma once
 
-#endif
+#include <stdlib.h>
+
+typedef struct {
+  size_t a;
+  size_t b;
+  size_t c;
+} triplet_t;
+
+typedef struct {
+  size_t count;
+  triplet_t triplets[];
+} triplets_t;
+
+triplets_t *triplets_with_sum(size_t sum);
+
+void free_triplets(triplets_t *triplets);
