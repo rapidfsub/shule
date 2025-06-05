ace4241fb37f1fb7576b3486774ecfbf79eb06cc
Minsub Kim <rapidfsub@gmail.com>
2025-06-05 16:33:04 +0900

    fix test using postgres data layer

--- emil/test/emil/expression_test.exs
+++ emil/test/emil/expression_test.exs
@@ -1,57 +1,17 @@
 alias Emil.ExpressionTest, as: ThisTest
 use Emil.TestPrelude
 
-defmodule ThisTest.DateTime do
-  def utc_now() do
-    ~U[2000-01-01 00:00:00Z]
-  end
-
-  def seoul_now() do
-    DateTime.shift_zone!(utc_now(), "Asia/Seoul")
-  end
-end
-
-defmodule ThisTest.Obj do
-  use Ash.Resource,
-    domain: TestDomain,
-    data_layer: Ash.DataLayer.Ets
-
-  actions do
-    defaults [:read, create: :*]
-  end
-
-  preparations do
-    prepare build(load: [:is_utc_active, :is_seoul_active])
-  end
-
-  attributes do
-    uuid_v7_primary_key :id
-    attribute :expires_at, :utc_datetime, allow_nil?: false, public?: true
-  end
-
-  calculations do
-    calculate :is_utc_active, :boolean do
-      calculation expr(expires_at > lazy({ThisTest.DateTime, :utc_now, []}))
-    end
-
-    calculate :is_seoul_active, :boolean do
-      calculation expr(expires_at > lazy({ThisTest.DateTime, :seoul_now, []}))
-    end
-  end
-end
-
 defmodule ThisTest do
-  use ExUnit.Case, async: true
+  use Emil.TestDataCase, async?: true
 
-  test "hey" do
-    params = %{expires_at: ~U[2000-01-01 00:15:00Z]}
+  test "should be equal even if the time zone is different" do
+    params = %{expires_at: ~U[2000-01-02 00:15:00Z]}
 
     obj =
-      Changeset.for_create(ThisTest.Obj, :create, params)
+      Changeset.for_create(Emil.SimpleDomain.Token, :create, params)
       |> Ash.create!()
       |> Ash.load!([:is_utc_active, :is_seoul_active])
 
-    assert true = obj.is_utc_active
-    assert true = obj.is_seoul_active
+    assert obj.is_utc_active == obj.is_seoul_active
   end
 end
