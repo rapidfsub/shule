6a223fee4f77eb4a78c923bd5d756aa29acd8339
Minsub Kim <rapidfsub@gmail.com>
2025-06-05 09:45:50 +0900

    mix igniter.install ash

--- void/mix.exs
+++ void/mix.exs
@@ -22,6 +22,8 @@ defmodule Void.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:sourceror, "~> 1.8", only: [:dev, :test]},
+      {:ash, "~> 3.0"},
       {:igniter, "~> 0.6", only: [:dev, :test]}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
