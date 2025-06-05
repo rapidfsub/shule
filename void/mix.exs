6c0d2e6fbe551b297b340947590dba64721c19c2
Minsub Kim <rapidfsub@gmail.com>
2025-06-05 09:58:37 +0900

    mix ash_authentication.add_strategy password

--- void/mix.exs
+++ void/mix.exs
@@ -25,6 +25,7 @@ defmodule Void.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:bcrypt_elixir, "~> 3.0"},
       {:simple_sat, "~> 0.1"},
       {:picosat_elixir, "~> 0.2"},
       {:ash_authentication, "~> 4.0"},
