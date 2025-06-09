44b8c898eebcf76f4d99dca32dd27d3937b95d90
Minsub Kim <rapidfsub@gmail.com>
2025-06-09 15:35:10 +0900

    render previous names

--- tunez/lib/tunez_web/live/artists/show_live.ex
+++ tunez/lib/tunez_web/live/artists/show_live.ex
@@ -27,6 +27,9 @@ defmodule TunezWeb.Artists.ShowLive do
         <.h1>
           {@artist.name}
         </.h1>
+        <:subtitle :if={not Enum.empty?(@artist.previous_names)}>
+          formerly known as: {Enum.join(@artist.previous_names, ", ")}
+        </:subtitle>
         <:action>
           <.button_link
             kind="error"
