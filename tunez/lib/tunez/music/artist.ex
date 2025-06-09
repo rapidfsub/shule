684d1f84341303ef158bb502ec40135f4f61e3a9
Minsub Kim <rapidfsub@gmail.com>
2025-06-09 15:28:23 +0900

    refactor

--- tunez/lib/tunez/music/artist.ex
+++ tunez/lib/tunez/music/artist.ex
@@ -20,21 +20,7 @@ defmodule Tunez.Music.Artist do
       primary? true
       require_atomic? false
       accept [:name, :biography]
-
-      change fn changeset, _context ->
-        new_name = Ash.Changeset.get_attribute(changeset, :name)
-        previous_name = Ash.Changeset.get_data(changeset, :name)
-        previous_names = Ash.Changeset.get_data(changeset, :previous_names)
-
-        names =
-          [previous_name | previous_names]
-          |> Enum.uniq()
-          |> Enum.reject(fn name -> name == new_name end)
-
-        Ash.Changeset.change_attribute(changeset, :previous_names, names)
-      end do
-        where changing(:name)
-      end
+      change Tunez.Music.Changes.UpdatePreviousNames, where: changing(:name)
     end
 
     destroy :destroy do
