e00730b72d614f8afa9e4cdb52adc4f8d7405c55
Minsub Kim <rapidfsub@gmail.com>
2025-06-09 19:16:19 +0900

    remove unwanted extras

--- tunez/lib/tunez/music/artist.ex
+++ tunez/lib/tunez/music/artist.ex
@@ -79,5 +79,13 @@ defmodule Tunez.Music.Artist do
 
   graphql do
     type :artist
+
+    filterable_fields [
+      :album_count,
+      :cover_image_url,
+      :inserted_at,
+      :latest_album_year_released,
+      :updated_at
+    ]
   end
 end
