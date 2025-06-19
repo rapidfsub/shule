3ae87b67443d84693375d27a0ca83bcfa8a812be
Minsub Kim <rapidfsub@gmail.com>
2025-06-19 11:11:50 +0900

    add guide and registry

---
+++ vomit/lib/vomit/registry.ex
@@ -0,0 +1,24 @@
+defmodule Vomit.Registry do
+  @type spec() :: [guide: module(), path: binary(), module: binary(), args: [term()]]
+  @callback specs() :: [spec()]
+
+  defmacro __using__(opts) do
+    dirname = __CALLER__.file |> Path.dirname()
+
+    specs =
+      for spec <- Keyword.fetch!(opts, :specs) do
+        Keyword.update!(spec, :path, fn path ->
+          Path.join(dirname, path) |> Path.expand()
+        end)
+      end
+
+    quote do
+      @behaviour Vomit.Registry
+
+      @impl Vomit.Registry
+      def specs() do
+        unquote(specs)
+      end
+    end
+  end
+end
