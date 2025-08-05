64b9dde150b195b3aec5b8c281a71844f520fa72
Minsub Kim <rapidfsub@gmail.com>
2025-08-05 12:01:16 +0900

    move mixin

--- eva/apps/mixin/lib/mixin.ex
+++ eva/apps/mixin/lib/mixin.ex
@@ -1,18 +1,50 @@
 defmodule Mixin do
-  @moduledoc """
-  Documentation for `Mixin`.
-  """
+  defp list_delegates(mod, fname, opts, pred) do
+    {mod, _bindings} = Code.eval_quoted(mod)
+    arity_or_nil = Keyword.get(opts, :arity)
 
-  @doc """
-  Hello world.
+    for info <- __MODULE__.FunInfo.list_infos(mod, fname), pred.(info) do
+      dname = Keyword.get(opts, :alias, info.name)
+      opts = [to: info.mod, as: info.name]
 
-  ## Examples
+      for arity <- (info.arity - info.defaults)..info.arity//1, arity_or_nil in [nil, arity] do
+        args = Enum.take(info.args, arity)
 
-      iex> Mixin.hello()
-      :world
+        [
+          quote do
+            if Module.defines?(__MODULE__, unquote({dname, arity})) do
+              raise "#{unquote(inspect({dname, arity}))} already defined"
+            end
+          end,
+          if info.doc do
+            quote do
+              @doc unquote(info.doc)
+            end
+          end,
+          quote do
+            defdelegate unquote({dname, [], args}), unquote(opts)
+          end
+        ]
+      end
+    end
+  end
 
-  """
-  def hello do
-    :world
+  defmacro mixin(mod, opts \\ []) do
+    only = Keyword.get(opts, :only, nil)
+    except = Keyword.get(opts, :except, [])
+
+    list_delegates(mod, nil, opts, fn info ->
+      (!only || info.name in only) && info.name not in except
+    end)
+  end
+
+  defmacro delegate_to(mod, fname, opts \\ []) do
+    list_delegates(mod, fname, opts, &Function.identity/1)
+  end
+
+  defmacro __using__(_opts) do
+    quote do
+      import Mixin, only: [mixin: 1, mixin: 2, delegate_to: 2, delegate_to: 3]
+    end
   end
 end
---
+++ eva/apps/mixin/lib/mixin/fun_info.ex
@@ -0,0 +1,80 @@
+defmodule Mixin.FunInfo do
+  @enforce_keys [:mod, :name, :arity, :defaults, :args, :doc]
+  defstruct @enforce_keys
+
+  def list_infos(mod, name) do
+    case Code.fetch_docs(mod) do
+      {:docs_v1, _anot, _beam_language, _format, _module_doc, _meta, docs} ->
+        for doc_element <- list_doc_elements(docs, name) do
+          %__MODULE__{
+            mod: mod,
+            name: get_name(doc_element),
+            arity: get_arity(doc_element),
+            defaults: get_defaults(doc_element),
+            args: list_args(doc_element),
+            doc: get_doc(doc_element)
+          }
+        end
+
+      {:error, _reason} ->
+        for {mod, name, arity} <- list_mfas(mod, name) do
+          %__MODULE__{
+            mod: mod,
+            name: name,
+            arity: arity,
+            defaults: 0,
+            args: list_dummy_args(arity),
+            doc: nil
+          }
+        end
+    end
+  end
+
+  defp list_doc_elements(docs, name) do
+    for {{:function, f, _a}, _anot, _sigs, _doc_content, _meta} = element <- docs,
+        name in [nil, f] and not String.starts_with?(to_string(f), "__") do
+      element
+    end
+  end
+
+  defp get_name({{:function, f, _a}, _anot, _sigs, _doc_content, _meta}) do
+    f
+  end
+
+  defp get_arity({{:function, _f, a}, _anot, _sigs, _doc_content, _meta}) do
+    a
+  end
+
+  defp get_defaults({{:function, _f, _a}, _anot, _sigs, _doc_content, meta}) do
+    Map.get(meta, :defaults, 0)
+  end
+
+  defp list_args({{:function, _f, _a}, _anot, [sig], _doc_content, _meta}) do
+    {_f, _meta, args} = Code.string_to_quoted!(sig)
+
+    Enum.map(args, fn
+      {:\\, _meta, [arg, _default]} -> arg
+      arg -> arg
+    end)
+  end
+
+  defp get_doc({{:function, _f, _a}, _anot, _sigs, doc_content, _meta}) do
+    case doc_content do
+      %{"en" => doc} -> doc
+      :none -> nil
+      :hidden -> nil
+    end
+  end
+
+  defp list_mfas(mod, name) do
+    for {f, a} <- mod.__info__(:functions), name in [nil, f] do
+      {mod, f, a}
+    end
+  end
+
+  defp list_dummy_args(arity) do
+    for arity <- 1..arity//1 do
+      Macro.var(:"x#{arity}", nil)
+    end
+  end
+end
