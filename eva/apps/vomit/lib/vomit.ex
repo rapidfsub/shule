6bc4701e1c5ad2c73b44022a977c37ca5d98eb04
Minsub Kim <rapidfsub@gmail.com>
2025-08-05 12:29:53 +0900

    mix new vomit

---
+++ eva/apps/vomit/lib/vomit.ex
@@ -0,0 +1,18 @@
+defmodule Vomit do
+  @moduledoc """
+  Documentation for `Vomit`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Vomit.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
