4e619e6e508b83dce337ce06ad0300c2ef4381f4
Minsub Kim <rapidfsub@gmail.com>
2025-08-05 14:02:38 +0900

    mix new vomitex

---
+++ eva/apps/vomitex/lib/vomitex.ex
@@ -0,0 +1,18 @@
+defmodule Vomitex do
+  @moduledoc """
+  Documentation for `Vomitex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Vomitex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
