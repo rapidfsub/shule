86f644d1ad17d2828031dfbbc775c6b98eb9dab1
Minsub Kim <rapidfsub@gmail.com>
2025-08-08 10:29:45 +0900

    mix igniter.install explorer

--- simons/mix.exs
+++ simons/mix.exs
@@ -7,7 +7,7 @@ defmodule Simons.MixProject do
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: Enum.concat([deps(), dev_deps()])
     ]
   end
 
@@ -21,9 +21,13 @@ defmodule Simons.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:igniter, "~> 0.6", only: [:dev, :test]}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:explorer, ">= 0.0.0"}
+    ]
+  end
+
+  defp dev_deps() do
+    [
+      {:igniter, ">= 0.0.0", only: [:dev, :test]}
     ]
   end
 end
