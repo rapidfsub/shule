00821bc5ffebf6e42b7ed3ac0ad3ee6ddd87e71b
Minsub Kim <rapidfsub@gmail.com>
2025-08-13 17:06:38 +0900

    refactor crawler

--- simons/lib/simons/krx_data/crawler.ex
+++ simons/lib/simons/krx_data/crawler.ex
@@ -2,55 +2,42 @@ defmodule Simons.KrxData.Crawler do
   use Simons.KrxData.Prelude
 
   def fetch_profiles() do
-    for isins <- Simons.KrxData.DataFrameKit.get_isins() |> Enum.chunk_every(500) do
-      results = do_fetch_profiles(isins)
+    crawl(fn isin ->
+      path = FileKit.get_profile_path(isin)
 
-      if Enum.any?(results, &(&1 == :ok)) do
-        for _ <- 1..6 do
-          sleep(10)
-        end
+      if File.exists?(path) do
+        {:error, :already_exists}
       else
-        {:error, :no_sleep}
+        resp = ApiClient.get_profile(isin)
+
+        resp.body
+        |> List.wrap()
+        |> DataFrame.new()
+        |> DataFrame.to_csv!(path)
       end
-    end
-  end
-
-  defp do_fetch_profiles(isins) do
-    for isins <- Enum.chunk_every(isins, 5) do
-      results =
-        for isin <- isins do
-          Task.async(fn ->
-            path = FileKit.get_profile_path(isin)
-
-            if File.exists?(path) do
-              {:error, :already_exists}
-            else
-              resp = ApiClient.get_profile(isin)
-
-              resp.body
-              |> List.wrap()
-              |> DataFrame.new()
-              |> DataFrame.to_csv!(path)
-            end
-          end)
-        end
-        |> Task.await_many()
-
-      inspect(event_name: :fetch, attributes: [isins: isins, results: results]) |> Logger.info()
-
-      if Enum.any?(results, &(&1 == :ok)) do
-        sleep(1)
-      else
-        {:error, :no_sleep}
-      end
-    end
+    end)
   end
 
   def fetch_candles(s_date, e_date) do
-    for isins <- Simons.KrxData.DataFrameKit.get_isins() |> Enum.chunk_every(500) do
-      results = do_fetch_candles(isins, s_date, e_date)
+    crawl(fn isin ->
+      path = FileKit.get_candles_path(isin, s_date.year)
 
-      if Enum.any?(results, &(&1 == :ok)) do
+      if File.exists?(path) do
+        {:error, :already_exists}
+      else
+        resp = ApiClient.get_candles(isin, s_date, e_date)
+
+        resp.body
+        |> Map.fetch!("output")
+        |> DataFrame.new()
+        |> DataFrame.to_csv!(path)
+      end
+    end)
+  end
+
+  defp crawl(fun) do
+    for isins <- DataFrameKit.get_isins() |> Enum.chunk_every(500) do
+      if :ok in do_crawl(isins, fun) do
         for _ <- 1..6 do
           sleep(10)
         end
@@ -60,30 +47,19 @@ defmodule Simons.KrxData.Crawler do
     end
   end
 
-  defp do_fetch_candles(isins, s_date, e_date) do
+  defp do_crawl(isins, fun) do
     for isins <- Enum.chunk_every(isins, 5) do
       results =
         for isin <- isins do
           Task.async(fn ->
-            filename = FileKit.get_candles_path(isin, s_date.year)
-
-            if File.exists?(filename) do
-              {:error, :already_exists}
-            else
-              resp = ApiClient.get_candles(isin, s_date, e_date)
-
-              resp.body
-              |> Map.fetch!("output")
-              |> DataFrame.new()
-              |> DataFrame.to_csv!(filename)
-            end
+            fun.(isin)
           end)
         end
-        |> Task.await_many()
+        |> Task.await_many(:timer.minutes(1))
 
-      inspect(event_name: :fetch, attributes: [isins: isins, results: results]) |> Logger.info()
+      inspect(event_name: :crawl, attributes: [isins: isins, results: results]) |> Logger.info()
 
-      if Enum.any?(results, &(&1 == :ok)) do
+      if :ok in results do
         sleep(1)
       else
         {:error, :no_sleep}
