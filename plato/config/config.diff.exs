ab902a46835e2c455c7ca240067947276ea68ca7
Minsub Kim <rapidfsub@gmail.com>
2025-09-20 13:19:16 +0900

    fix config

--- plato/config/config.diff.exs
+++ plato/config/config.diff.exs
@@ -3,5 +3,7 @@ import Config
 # Ash configuration
 config :plato, ash_domains: [Plato.Domain]
 
-# Ash Postgres configuration  
-config :ash, :validate_domain_config_inclusion?, false
+# Ash Postgres configuration
+config :ash,
+  validate_domain_config_inclusion?: false,
+  missed_notifications: :ignore
